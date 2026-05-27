03ffeec0084b18498874295b91583734bde2690b
Alisa-gh2 <154425195+Alisa-gh2@users.noreply.github.com>
2026-05-27 17:15:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -15,18 +15,16 @@ CLEAN_DOCS_DIR = "./clean_docs"
 INDEX_DIR = "./faiss_index"
 LOG_DIR = "./logs"
 
-# чанкинг 
-CHUNK_SIZE = 512
-OVERLAP = 64
+# чанкинг
+CHUNK_SIZE = 1024
+OVERLAP = 128
 
 # поиск
 TOP_K = 5
 
 # модель эмбеддингов (лёгкая, быстрая)
-# EMBEDDING_MODEL = "all-MiniLM-L6-v2"
-
-# модель эмбеддингов (помощнее, но может подвисать)
-EMBEDDING_MODEL = "paraphrase-multilingual-MiniLM-L12-v2"
+EMBEDDING_MODEL = "all-MiniLM-L6-v2"
+# EMBEDDING_MODEL = "paraphrase-multilingual-MiniLM-L12-v2"
 # воспроизводимость
 SEED = 42
 
